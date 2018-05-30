6dd6963672b948cf311d90dbe79f77f3b4d7fffa
Bilawal Liaqat <bilawal.liaqat@nxb.com.pk>
2018-05-30 18:36:11 +0500

    implemented Firebase SignIn & SignUp functionality

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
-import { TextInput } from 'react-native';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button , Card , CardSection, Input} from './common';
 
 
 class LoginForm extends Component{
-state = {email: '', password : ''};
+state = {email: '', password : '', error: ''};
+
+
+    onButtonPress(){
+        const { email , password} = this.state;
+        this.setState({ error: ''});
+
+        firebase.auth().signInWithEmailAndPassword(email , password)
+        .catch(() => {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(() =>{
+                this.setState({error: 'Authentication failed.'});
+            });
+        });
+
+    }
+
+
     render(){
         return(
             <Card>
@@ -28,9 +46,15 @@ state = {email: '', password : ''};
                     onChangeText = {password => this.setState( { password})} 
                     />
                 </CardSection>
-
+                <Text
+                style = {styles.errorTextStyle}
+                >
+                {this.state.error}
+                </Text>
                 <CardSection>
-                    <Button>
+                    <Button
+                    onPress= { this.onButtonPress.bind(this)}
+                    >
                         Login
                     </Button>
                 </CardSection>
@@ -42,6 +66,20 @@ state = {email: '', password : ''};
         );
     }
 }
+
+
+
+
+
+const styles = {
+
+    errorTextStyle: {
+        fontSize : 20,
+        alignSelf: 'center',
+        color: 'red'
+
+    }
+};
 
 
 export default LoginForm;
