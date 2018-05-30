ec7cf2593d72f4c2bfb02d446f5be0bd00f6c857
Bilawal Liaqat <bilawal.liaqat@nxb.com.pk>
2018-05-30 15:22:38 +0500

    Login Button component added.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import { View , Text } from 'react-native';
-import { Button, Header } from  './components/common'
+import { Button, Header } from  './components/common';
+import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
+
 class App extends Component {
+
+ componentWillMount(){
+
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCgni_zsIazhbUTbbx8ypgMB2VdeURQG2',
+    authDomain: 'authentication-d7054.firebaseapp.com',
+    databaseURL: 'https://authentication-d7054.firebaseio.com',
+    projectId: 'authentication-d7054',
+    storageBucket: 'authentication-d7054.appspot.com',
+    messagingSenderId: '811956573810'
+  });
+ }
+
 
   render(){
     return (
       <View>
         <Header headerText= "Authentication App"> </Header>
-        <Text>Auth app using React</Text>
+        <LoginForm />
       
       </View>
     );
